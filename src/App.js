6eb755d65ccfa45986acb4a521e4fc9db3c59bb6
Sam Narciso <samnarciso@USF-Guest-Wifi-129-99.laptops.usf.edu>
2023-10-30 15:25:59 -0400

    functional game with no tests

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import "./App.css";
 /** Simple app that just shows the LightsOut game. */
 
 function App() {
+  // Define the number of rows, columns, and chance of lights starting on
+  const nrows = 5; // You can adjust this as needed
+  const ncols = 5; // You can adjust this as needed
+  const chanceLightStartsOn = 0.25; // You can adjust this as needed
+
   return (
-      <div className="App">
-        <Board />
-      </div>
+    <div className="App">
+      <Board nrows={nrows} ncols={ncols} chanceLightStartsOn={chanceLightStartsOn} />
+    </div>
   );
 }
 
--- src/Board.js
+++ src/Board.js
@@ -1,73 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cell from "./Cell";
 import "./Board.css";
-
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
 
 function Board({ nrows, ncols, chanceLightStartsOn }) {
   const [board, setBoard] = useState(createBoard());
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
   function createBoard() {
     let initialBoard = [];
-    // TODO: create array-of-arrays of true/false values
+    for (let i = 0; i < nrows; i++) {
+      let newRow = [];
+      for (let j = 0; j < ncols; j++) {
+        if (Math.random() > chanceLightStartsOn) {
+          newRow.push(true);
+        } else {
+          newRow.push(false);
+        }
+      }
+      initialBoard.push(newRow);
+    }
     return initialBoard;
   }
 
-  function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
-  }
-
+  // Handle cell clicks (flipping cells)
   function flipCellsAround(coord) {
-    setBoard(oldBoard => {
+    setBoard((oldBoard) => {
       const [y, x] = coord.split("-").map(Number);
-
+  
       const flipCell = (y, x, boardCopy) => {
-        // if this coord is actually on board, flip it
-
         if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
           boardCopy[y][x] = !boardCopy[y][x];
         }
       };
-
-      // TODO: Make a (deep) copy of the oldBoard
-
-      // TODO: in the copy, flip this cell and the cells around it
-
-      // TODO: return the copy
+  
+      const boardCopy = JSON.parse(JSON.stringify(oldBoard)); // Create a deep copy
+  
+      // Flip the selected cell and cells around it
+      flipCell(y, x, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+  
+      return boardCopy;
     });
   }
+  
 
-  // if the game is won, just show a winning msg & render nothing else
+  // Check for a win condition whenever the board changes
+  useEffect(() => {
+    function checkWin() {
+      for (let i = 0; i < nrows; i++) {
+        for (let j = 0; j < ncols; j++) {
+          if (board[i][j]) {
+            return false; // If any cell is turned on, the player hasn't won yet.
+          }
+        }
+      }
+      // If all cells are turned off, the player has won.
+      return true;
+    }
+  
+    if (checkWin()) {
+      alert("Congratulations! You've won!");
+    }
+  }, [board, nrows, ncols]);
 
-  // TODO
-
-  // make table board
-
-  // TODO
+  // Render the game board
+  return (
+    <table className="Board">
+      <tbody>
+        {board.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, colIndex) => (
+              <Cell
+                key={`${rowIndex}-${colIndex}`}
+                flipCellsAroundMe={() => flipCellsAround(`${rowIndex}-${colIndex}`)}
+                isLit={cell}
+              />
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Board;
